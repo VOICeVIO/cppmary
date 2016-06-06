afb197239a5c2575eaaeecc0cd7a63348aa1810a
sooda <liushouda@qq.com>
2016-06-06 15:51:37 +0800

    add c++ string util function; AUX_SOURCE_DIRECTORY not work

---
+++ include/common/stringUtil.h
@@ -0,0 +1,31 @@
+//
+// Created by sooda on 16/6/6.
+//
+
+#ifndef CPPMARY_STRINGUTIL_H
+#define CPPMARY_STRINGUTIL_H
+
+//
+// Created by sooda on 16/6/6.
+//
+#include <string>
+#include "common/stringUtil.h"
+#include <vector>
+
+namespace cppmary {
+
+   std::vector <std::string> split(const std::string &text, char sep);
+
+   // trim from start
+   std::string& ltrim(std::string & s);
+
+// trim from end
+   std::string& rtrim(std::string & s);
+
+// trim from both ends
+   std::string& trim(std::string & s);
+
+}
+
+
+#endif //CPPMARY_STRINGUTIL_H
---
+++ src/common/stringUtil.cpp
@@ -0,0 +1,37 @@
+//
+// Created by sooda on 16/6/6.
+//
+#include "common/stringUtil.h"
+#include <vector>
+#include <string>
+#include <functional>
+namespace cppmary {
+
+   std::vector <std::string> split(const std::string &text, char sep) {
+      std::vector <std::string> tokens;
+      std::size_t start = 0, end = 0;
+      while ((end = text.find(sep, start)) != std::string::npos) {
+         tokens.push_back(text.substr(start, end - start));
+         start = end + 1;
+      }
+      tokens.push_back(text.substr(start));
+      return tokens;
+   }
+
+// trim from start
+   std::string &ltrim(std::string &s) {
+      s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+      return s;
+   }
+
+// trim from end
+   std::string &rtrim(std::string &s) {
+      s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+      return s;
+   }
+
+// trim from both ends
+   std::string &trim(std::string &s) {
+      return ltrim(rtrim(s));
+   }
+}
