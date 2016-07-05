c682e27b0d2f489bbf22ee560ebeb4def1f672d7
sooda <liushouda@qq.com>
2016-07-05 17:10:00 +0800

    ReferenceCounted使用注意点： 1. 不要将局部作用域的指针传到别的函数中，否则会导致引用计数不一致：ReleaseRef和退出作用域后，双重释放内存 2. 只有对象指针有可能传到别的函数中，就应该使用引用计数管理： 在实例化处Addref，在退出作用域ReleaseRef。 否则会导致ReleaseRef和delete 对象混用，导致双重释放。

--- test/module_test.cpp
+++ test/module_test.cpp
@@ -209,7 +209,12 @@ void HtsEngineTest() {
 void totalTest() {
     std::string rawXml = cppmary::TextToMaryXml::getInstance().process("这个世界上，为什么只有我这么帅呢?");
     //std::string rawXml = cppmary::TextToMaryXml::getInstance().process("物流信息都显示已经在晋江揽件了，还让我整整等了你四天，你们快递公司是干什么吃的！为什么只有意干什么?"); //这句有时候出错,有时候不出错.可用于调试.
-    JiebaSegment::Instance()->LoadResource("data/jieba/");
+    std::string jieba_buffer = getFileString("data/jieba/jieba.dict.utf8");
+    std::string hmm_model_buffer = getFileString("data/jieba/hmm_model.utf8");;
+    std::string user_buffer = getFileString("data/jieba/user.dict.utf8");
+    JiebaSegment::Instance()->LoadResource(jieba_buffer,
+                                           hmm_model_buffer,
+                                           user_buffer);
     cppmary::InterModules* tokenizer = new cppmary::Tokenizer();
     std::string tokenStr = tokenizer->process(rawXml);
     std::string sylDictName = "test/pinyin_han.txt";
@@ -234,14 +239,15 @@ void totalTest() {
     result = doc1.load_string(allophoneSetStr.c_str());
     std::string allosetStr = MaryXml::saveDoc2String(doc1);
 
-    FeatureProcessorManager manager("zh", allosetStr);
-    manager.AddRef();
+    FeatureProcessorManager *manager = new FeatureProcessorManager("zh", allosetStr);
+    manager->AddRef();
     //TargetFeatureComputer featureComputer(manager, "phrase_numsyls phone phrase_zhtone prevprev_zhtone prev_zh_tone zh_tone next_zhtone nextnext_zh_tone pos tobi_accent prev_accent prevprev_tobi_accent next_accent nextnext_tobi_accent accented accented_syls_from_phrase_start accented_syls_from_phrase_end syls_from_prev_accent");
     //TargetFeatureComputer featureComputer(manager, "phone prev_phone prev_prev_phone next_phone next_next_phone phrase_numsyls phone phrase_zhtone prevprev_zhtone prev_zh_tone zh_tone next_zhtone nextnext_zh_tone pos tobi_accent prev_accent prevprev_tobi_accent next_accent nextnext_tobi_accent accented accented_syls_from_phrase_start accented_syls_from_phrase_end syls_from_prev_accent syl_break breakindex phrase_numsyls zh_tone segs_from_syl_start segs_from_syl_end syls_from_phrase_start syls_from_phrase_end syl_break is_pause words_from_phrase_start words_from_phrase_end words_from_sentence_start words_from_sentence_end phrases_from_sentence_start phrases_from_sentence_end sentence_punc next_punctuation prev_punctuation words_to_next_punctuation words_from_prev_punctuation position_type tobi_accent");
     //TargetFeatureComputer featureComputer(manager, "phone prev_phone prev_prev_phone next_phone next_next_phone  next_zhtone breakindex word_numsegs prev_is_pause words_from_phrase_end sentence_numwords accented_syls_from_phrase_end next_is_pause prev_zh_tone segs_from_word_start syls_from_phrase_end prev_syl_break position_type next_punctuation syls_to_next_accent segs_from_word_end phrase_numsyls prev_punctuation syls_from_word_start words_from_sentence_end phrases_from_sentence_start tobi_accent word_numsyls segs_from_syl_end phrase_numwords zh_tone segs_from_syl_start next_tobi_accent syl_break prev_pos sentence_punc nextnext_zh_tone phrases_from_sentence_end accented_syls_from_phrase_start syls_from_prev_accent pos words_from_prev_punctuation words_from_sentence_start syls_from_word_end syl_numsegs next_pos accented next_accent words_to_next_punctuation prev_accent syls_from_phrase_start nextnext_tobi_accent sentence_numphrases words_from_phrase_start");
     std::string trickyName = "test/trickyPhones.txt";
     std::string trickyStr = getFileString(trickyName);
     PhoneTranslator* phoneTranslator = new PhoneTranslator(trickyStr);
+    phoneTranslator->AddRef();
     std::string featureMapName = "test/hmmFeaturesMapNew.txt";
     std::vector<string> featureName;
     std::vector<string> featureAlias;
@@ -253,11 +259,11 @@ void totalTest() {
     contextFeatureName.push_back("next_phone");
     contextFeatureName.push_back("next_next_phone");
     //TargetFeatureComputer featureComputer(manager, contextFeatureName);
-    TargetFeatureComputer* featureComputer = new TargetFeatureComputer(&manager, contextFeatureName);
+    TargetFeatureComputer* featureComputer = new TargetFeatureComputer(manager, contextFeatureName);
     featureComputer->AddRef();
     std::cout << "context feature size: " << contextFeatureName.size() << std::endl;
     assert(featureName.size() == featureAlias.size());
-    InterModules* label = new LabelGenerator(&manager, featureComputer, featureName, featureAlias, phoneTranslator);
+    InterModules* label = new LabelGenerator(manager, featureComputer, featureName, featureAlias, phoneTranslator);
     label->AddRef();
     std::string labelString = label->process(pronunStr);
     std::string modelName = "test/labixx23.htsvoice";
@@ -267,15 +273,12 @@ void totalTest() {
     //featureComputer->ReleaseRef();
     label->ReleaseRef();
     featureComputer->ReleaseRef();
-    std::cout << manager.Refs() << std::endl;
-    //manager.ReleaseRef();
-    std::cout << "ddd" << std::endl;
-//    delete label;
-//    delete htsengine;
-//    delete phoneTranslator;
-//    delete pronuciation;
-//    delete phonemiser;
-//    delete tokenizer;
+    manager->ReleaseRef();
+    delete htsengine;
+    phoneTranslator->ReleaseRef();
+    delete pronuciation;
+    delete phonemiser;
+    delete tokenizer;
 }
 
 int main() {
