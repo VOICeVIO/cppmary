cf2611216790c2ecfbe4aefa34075f94e9684f87
sooda <liushouda@qq.com>
2016-07-12 08:18:09 +0800

    reformat; add error log report

--- include/common/JiebaSegment.h
+++ include/common/JiebaSegment.h
@@ -7,30 +7,40 @@
 //
 #ifndef WORDS_PROCESS_H
 #define WORDS_PROCESS_H
+
 #include "stdio.h"
 #include <vector>
 #include <string>
 #include <utility>
-class JiebaSegment{
+
+class JiebaSegment {
 public:
-    static JiebaSegment* Instance();
+    static JiebaSegment *Instance();
+
     void LoadResource(std::string dir);
+
     void LoadResource(std::string &jieba_buffer,
                       std::string &hmm_model_buffer,
                       std::string &user_buffer);
-    void Cut(const std::string& sentence, std::vector<std::string>& words);
-    void Tag(const std::string& sentence, std::vector<std::pair<std::string, std::string> >& wordTags);
-    void Extract(const std::string& sentence,std::vector<std::string>&words,int topN=1);
-    void InsertNewWords(std::vector<std::string>& words);
+
+    void Cut(const std::string &sentence, std::vector<std::string> &words);
+
+    void Tag(const std::string &sentence, std::vector<std::pair<std::string, std::string> > &wordTags);
+
+    void Extract(const std::string &sentence, std::vector<std::string> &words, int topN = 1);
+
+    void InsertNewWords(std::vector<std::string> &words);
+
 protected:
-    JiebaSegment(void)
-    {
-        _app=NULL;
+    JiebaSegment(void) {
+        _app = NULL;
     }
-    virtual ~JiebaSegment(void)
-    {
+
+    virtual ~JiebaSegment(void) {
     }
-    static JiebaSegment* _instance;
-    void* _app;
+
+    static JiebaSegment *_instance;
+    void *_app;
 };
+
 #endif
--- src/common/JiebaSegment.cpp
+++ src/common/JiebaSegment.cpp
@@ -4,54 +4,55 @@
 
 #include "common/JiebaSegment.h"
 #include "cppjieba/Jieba.hpp"
-JiebaSegment* JiebaSegment::_instance=NULL;
-JiebaSegment* JiebaSegment::Instance(){
-    if( 0== _instance)
-    {
+
+JiebaSegment *JiebaSegment::_instance = NULL;
+
+JiebaSegment *JiebaSegment::Instance() {
+    if (0 == _instance) {
         _instance = new JiebaSegment();
     }
     return _instance;
 }
 
-void JiebaSegment::LoadResource(std::string dir)
-{
-    if(_app!=NULL) {
-        return;
-    }
-
-    cppjieba::Jieba* app=new cppjieba::Jieba(dir+"/jieba.dict.utf8",
-                                   dir+"/hmm_model.utf8",
-                                   dir+"/user.dict.utf8");
-     _app=(cppjieba::Jieba*)app;
-}
-
-void JiebaSegment::LoadResource(std::string &jieba_buffer,
-                              std::string &hmm_model_buffer,
-                              std::string &user_buffer) {
+void JiebaSegment::LoadResource(std::string dir) {
     if (_app != NULL) {
         return;
     }
-    cppjieba::Jieba* app=new cppjieba::Jieba(jieba_buffer,
-                                              hmm_model_buffer,
-                                              user_buffer,
-                                              1);
-    _app=(cppjieba::Jieba*)app;
+
+    cppjieba::Jieba *app = new cppjieba::Jieba(dir + "/jieba.dict.utf8",
+                                               dir + "/hmm_model.utf8",
+                                               dir + "/user.dict.utf8");
+    _app = (cppjieba::Jieba *) app;
 }
 
-void JiebaSegment::Cut(const std::string &sentence, std::vector<std::string> &words)
-{
-    if(_app==NULL) {
+void JiebaSegment::LoadResource(std::string &jieba_buffer,
+                                std::string &hmm_model_buffer,
+                                std::string &user_buffer) {
+    if (_app != NULL) {
         return;
     }
-    cppjieba::Jieba* app=(cppjieba::Jieba*)_app;
+    cppjieba::Jieba *app = new cppjieba::Jieba(jieba_buffer,
+                                               hmm_model_buffer,
+                                               user_buffer,
+                                               1);
+    _app = (cppjieba::Jieba *) app;
+}
+
+void JiebaSegment::Cut(const std::string &sentence, std::vector<std::string> &words) {
+    if (_app == NULL) {
+        XLOG(ERROR) << "jieba have not been initialize";
+        return;
+    }
+    cppjieba::Jieba *app = (cppjieba::Jieba *) _app;
     app->Cut(sentence, words);
 }
 
-void JiebaSegment::Tag(const std::string& sentence, std::vector<std::pair<std::string, std::string> >& wordTags) {
-    if(_app==NULL) {
+void JiebaSegment::Tag(const std::string &sentence, std::vector<std::pair<std::string, std::string> > &wordTags) {
+    if (_app == NULL) {
+        XLOG(ERROR) << "jieba have not been initialize";
         return;
     }
-    cppjieba::Jieba* app=(cppjieba::Jieba*)_app;
+    cppjieba::Jieba *app = (cppjieba::Jieba *) _app;
     app->Tag(sentence, wordTags);
 }
 
@@ -65,15 +66,13 @@ void JiebaSegment::Tag(const std::string& sentence, std::vector<std::pair<std::s
 //    app->extract(sentence, words, topN);
 //}
 
-void JiebaSegment:: InsertNewWords(std::vector<std::string>& words)
-{
-    if(_app==NULL)
-    {
+void JiebaSegment::InsertNewWords(std::vector<std::string> &words) {
+    if (_app == NULL) {
+        XLOG(ERROR) << "jieba have not been initialize";
         return;
     }
-    cppjieba::Jieba* app=(cppjieba::Jieba*)_app;
-    for(int i=0;i<words.size();i++)
-    {
+    cppjieba::Jieba *app = (cppjieba::Jieba *) _app;
+    for (int i = 0; i < words.size(); i++) {
         app->InsertUserWord(words[i]);
     }
 }
